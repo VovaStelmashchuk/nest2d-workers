7442f9b592f8afa4c26ad4a8259dbd09b0c95888
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2025-06-08 12:22:00 +0300

    fix loggers

--- python/utils/logger.py
+++ python/utils/logger.py
@@ -4,7 +4,7 @@ from pythonjsonlogger import jsonlogger
 def setup_json_logger(name=None, level=logging.INFO):
     logger = logging.getLogger(name)
     logHandler = logging.StreamHandler()
-    formatter = jsonlogger.JsonFormatter()
+    formatter = jsonlogger.JsonFormatter('%(asctime)s %(levelname)s %(message)s')
     logHandler.setFormatter(formatter)
     if not logger.hasHandlers():
         logger.addHandler(logHandler)
